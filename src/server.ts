bfd64ed617208d7bd0f0ed3a3c1ec7a214d525b3
Bruno Pavese <bruno@ejcm.com.br>
2024-04-17 12:48:38 -0300

    ts e express configurados

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+
+async function bootstrap() {
+  const app = express()
+
+  app.use(express.json())
+
+  app.listen('3333', () => {
+    console.log(
+      `🚀 Server ready at: http://localhost:3333`,
+    )
+  })
+}
+
+bootstrap()
