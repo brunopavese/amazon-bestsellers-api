1ef3bb831d929638990282af43d105ede28502c6
Bruno Pavese <bruno@ejcm.com.br>
2024-04-18 01:53:18 -0300

    api configurada com o express

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,29 @@
-import express from 'express'
-import 'dotenv/config'
+import express, { Request, Response } from 'express'
 import { getAmazonBestsellerItems } from './utils/amazonScraper'
+import { List } from './utils/types'
+import 'dotenv/config'
 
 async function bootstrap() {
   const app = express()
+
+  app.get('/', async (req: Request, res: Response): Promise<void> => {
+    try {
+      const bestSellers: Array<List> | null = await getAmazonBestsellerItems()
+
+      if (bestSellers === null) {
+        console.error('An error occurred during scraping 🥲')
+        res.status(500).json({
+          message: 'An error occurred during scraping 🥲',
+        })
+      }
+
+      res.status(200).json({ bestSellers })
+    } catch {
+      res.status(500).json({
+        message: 'Error fetching Amazon Best Sellers',
+      })
+    }
+  })
 
   app.use(express.json())
 
@@ -12,14 +32,4 @@ async function bootstrap() {
   })
 }
 
-async function main() {
-  const offers = await getAmazonBestsellerItems()
-  if (offers) {
-    for (const item of offers) {
-      console.log(item)
-    }
-  }
-}
-
-main()
-// bootstrap()
+bootstrap()
